7ef3fcab0d392aab27089402678d8e2e52213262
SyL <yongliang@infinitetrinity.com.sg>
2023-10-06 21:32:33 +0800

    Fix header hydration error

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -5,15 +5,20 @@
         <img src="/images/logo.svg" alt="Logo" width="100" height="100" />
       </NuxtLink>
     </div>
-    <p class="mx-auto my-5 max-w-xl text-center">
-      <div class="font-semibold text-xl mb-2">An open source content by 
+    <div class="mx-auto my-5 max-w-xl text-center">
+      <div class="font-semibold text-xl mb-2">
+        An open source content by
         <NuxtLink class="text-orange-600" to="https://sgds.app">SGDS</NuxtLink>
       </div>
-      This is a starter blog created with Nuxt 3 Content and Tailwind CSS.<br/>
+      This is a starter blog created with Nuxt 3 Content and Tailwind CSS.<br />
       🌟 Check out this
-      <NuxtLink class="underline" to="https://sgds.app/articles/create-a-nuxt-3-content-blog-with-tailwind-css-in-one-hour">
+      <NuxtLink
+        class="underline"
+        to="https://sgds.app/articles/create-a-nuxt-3-content-blog-with-tailwind-css-in-one-hour"
+      >
         tutorial here to learn how we build it step by step.
-      </NuxtLink> 🌈
-    </p>
+      </NuxtLink>
+      🌈
+    </div>
   </header>
 </template>
