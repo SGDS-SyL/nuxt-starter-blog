7166a6b658c79ca820232d38bfa437ab47bcba4f
SyL <yongliang@infinitetrinity.com.sg>
2023-10-06 16:48:50 +0800

    Added article details for author and published date with moment

---
+++ components/ArticleDetails.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import moment from "moment";
+import { ParsedContent } from "@nuxt/content/dist/runtime/types";
+
+/* An article object will be passed over for us to use and it will 
+contain our Front-matter data including the author and date. */
+const props = defineProps<{
+  article: ParsedContent;
+}>();
+
+// Format the article date using the moment package we installed
+const publishedOn = computed(() => {
+  return props.article.date
+    ? moment(props.article.date).format("DD MMM YYYY")
+    : null;
+});
+</script>
+
+<template>
+  <div class="flex justify-between gap-12 lg:justify-start">
+    <div v-if="article.author">
+      <div class="mb-1 text-sm font-medium text-gray-500">Written by</div>
+      <div class="text-md font-medium text-gray-900">
+        {{ article.author }}
+      </div>
+    </div>
+    <div v-if="publishedOn">
+      <div class="mb-1 text-sm font-medium text-gray-500">Published on</div>
+      <div class="text-md font-medium">
+        {{ publishedOn }}
+      </div>
+    </div>
+  </div>
+</template>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,12 @@
 <template>
-  <main>
-    <ContentDoc />
+  <main class="my-20 prose max-w-5xl mx-auto">
+    <!-- prose class will style our content beautifully with Tailwind Typography -->
+    <ContentDoc v-slot="{ doc }">
+      <!-- Component by Nuxt content to fetche and provide us our article to use as doc -->
+      <ArticleDetails :article="doc" class="mb-5" />
+      <!-- Our ArticleDetails component -->
+      <ContentRenderer :value="doc" />
+      <!-- Component by Nuxt content to render our article -->
+    </ContentDoc>
   </main>
 </template>
