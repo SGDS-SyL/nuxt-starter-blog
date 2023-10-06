8820fd0c428df302aa982be217aa7bc1965913b3
SyL <yongliang@infinitetrinity.com.sg>
2023-10-06 22:55:27 +0800

    Added dark mode

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      children: `localStorage.getItem("vueuse-color-scheme") == 'dark' 
+    ? document.documentElement.classList.add("dark") 
+    : document.documentElement.classList.remove("dark");`,
+    },
+  ],
+});
+</script>
 <template>
   <NuxtLayout>
     <NuxtPage />
--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,3 +1,10 @@
+<script lang="ts" setup>
+import { SunIcon, MoonIcon } from "@heroicons/vue/24/solid";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
 <template>
   <header>
     <div class="mx-auto w-24">
@@ -19,6 +26,19 @@
         tutorial here to learn how we build it step by step.
       </NuxtLink>
       🌈
+    </div>
+    <div class="max-w-lg mx-auto flex justify-center my-3">
+      <button
+        class="rounded-md p-1.5 transition-colors hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-800"
+        aria-label="Change color mode"
+        @click="toggleDark()"
+      >
+        <client-only>
+          <!-- This is to make sure the icon is render only in client-side -->
+          <MoonIcon class="h-5 w-5" v-if="isDark" />
+          <SunIcon class="h-5 w-5" v-else />
+        </client-only>
+      </button>
     </div>
   </header>
 </template>
